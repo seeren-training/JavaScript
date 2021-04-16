3280d8a3d7285a39ff9a71a1f41e771c79b7c3e2
seeren <consultant@seeren.fr>
2021-04-16 23:00:27 +0200

    Add morpion

---
+++ morpion/README.md
@@ -0,0 +1,44 @@
+# Morpion Challenge
+
+Les intégrateur ont bien travaillé sur l'affichage d'un jeu de morpion mais leur compétence s'arrête là: ils emettent le souhaite suivant pour l'interface:
+
+* Entant que `timer` je peux être started, paused ou resumed afin qu'un joueur et un CPU s'affrontent.
+
+* Entant que `scorer` je peux comptabiliser les victoires du joueur et CPU afin que les résultats soient clairs.
+
+* Entant que `grille` le joueur et CPU peuvent choisir une case afin de remplire une ligne et remporter la victoire.
+
+* Entant que `grille` je posède un CPU qui souhaite gagner afin de challenger le joueur.
+
+---
+
+## Info techniques
+
+* En ajoutant la classe `circle` sur un élément possédant la classe `case` il aura un background avec un cercle bleu.
+
+* En ajoutant la classe `cross` sur un élément possédant la classe `case` il aura un background avec une croix rouge.
+
+* En ajoutant la classe `d-none` sur un élément il ne sera plus affiché.
+
+Les identifiants suivants sont disponibles:
+
+* timerStart
+* timerPause
+* timerResume
+* scorePlayer
+* scoreCPU
+* case1
+* case2
+* case3
+* case4
+* case5
+* case6
+* case7
+* case8
+* case9
+
+___
+
+## Objectif
+
+Découvrons les bases du langage JavaScript et utilisons le paradigme fonctionnel pour résoudre ce problème.
---
+++ morpion/webpack.config.js
@@ -0,0 +1,58 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './src/app.js',
+        './src/app.scss',
+    ],
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'app.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'file-loader',
+            },
+        ],
+    },
+    watchOptions: {
+        ignored: [
+            '/node_modules/',
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ 
+            filename: 'app.css' 
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            files: [
+                './index.html',
+            ],
+            server: {
+                baseDir: './',
+                middleware: (req, res, next) => (-1 === req.url.indexOf('.') && '/' !== req.url
+                    ? res.end(res.writeHead(302, { Location: '/' }))
+                    : next()),
+            },
+        }),
+    ],
+};
