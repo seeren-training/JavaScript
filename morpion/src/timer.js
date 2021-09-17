5f1f6b0491a452c5ba87d996ea2e78db406e7e79
seeren <consultant@seeren.fr>
2021-09-17 17:10:34 +0200

    Fix timer

--- morpion/src/timer.js
+++ morpion/src/timer.js
@@ -2,21 +2,35 @@ const timerTime = $('#timerTime');
 const btnStart = $('#timerStart');
 const btnPause = $('#timerPause');
 const btnResume = $('#timerResume');
+let intervalId = null;
+let second = 0;
+
+const increment = () => {
+    timerTime.text(new Date(second * 1000).toISOString().substr(14, 5));
+    second++;
+};
+
+const toggleBtn = (on, off) => {
+    on.toggleClass('d-block d-none');
+    off.toggleClass('d-block d-none');
+};
 
 const start = () => {
-    btnStart.toggleClass('d-block d-none');
-    btnPause.toggleClass('d-block d-none');
-};
-const pause = () => {
-    btnPause.toggleClass('d-block d-none');
-    btnResume.toggleClass('d-block d-none');
-};
-const resume = () => {
-    btnResume.toggleClass('d-block d-none');
-    btnPause.toggleClass('d-block d-none');
+    toggleBtn(btnStart, btnPause);
+    intervalId = setInterval(increment, 1000);
 };
 
-timerTime.text('00:00');
+const pause = () => {
+    toggleBtn(btnPause, btnResume);
+    clearInterval(intervalId);
+};
+
+const resume = () => {
+    toggleBtn(btnResume, btnPause);
+    intervalId = setInterval(increment, 1000);
+};
+
+increment();
 btnStart.on('click', start);
 btnPause.on('click', pause);
 btnResume.on('click', resume);
